cec6c6a5dd31d4f187b9a2c7b06962da1b3b5e7d
Yoan Ante <yoanante@gmail.com>
2021-01-02 22:07:57 -0500

    canonical url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://thirsty-minsky-44e768.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://www.mansfocus.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
