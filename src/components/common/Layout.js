a975a93b822c2a681b39b9ecae6c6a4810d0dba7
Yoan Ante <yoanante@gmail.com>
2021-01-07 21:28:44 -0500

    added gatsby to footer

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -86,7 +86,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © {new Date().getFullYear()} &mdash; Published with <a rel="nofollow" className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a> By <a rel="nofollow" className="site-foot-nav-item" href="https://yoanante.com" target="_blank">Yoan Ante</a>
+                                <Link to="/">{site.title}</Link> © {new Date().getFullYear()} &mdash; Published with <a rel="nofollow" className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a> Presented with <a href="https://www.gatsbyjs.com/">Gatsby.js</a> By <a rel="nofollow" className="site-foot-nav-item" href="https://yoanante.com" target="_blank">Yoan Ante</a>
                             </div>
                             <div className="site-foot-nav-right">
                                 <Navigation data={site.navigation} navClass="site-foot-nav-item" />
