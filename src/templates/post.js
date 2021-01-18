5c27d58968878a13cf57f92c5cf686c07ebcb8b0
Yoan Ante <yoanante@gmail.com>
2021-01-17 22:42:43 -0500

    quick and dirty light box

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
 import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
+import { set } from 'lodash'
 
 /**
 * Single post view (/:slug)
@@ -14,38 +15,59 @@ import { MetaData } from '../components/common/meta'
 */
 const Post = ({ data, location }) => {
     const post = data.ghostPost
+    const [galleryImg, setGalleryImg] = useState("")
+    const clickRecord = (e) => {
+        e.stopPropagation()
+        if (e.target.tagName === "IMG") {
+            const lbox = document.getElementById("lbox");
+            setGalleryImg(e.target.src)
+            lbox.style = "visibility: visible"
+        }
+    }
+    const removeLightBox = () => {
+        const lbox = document.getElementById("lbox");
+        lbox.style = "visibility: hidden"
+    }
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="article"
-            />
+            <MetaData data={data} location={location} type="article" />
             <Helmet>
                 <style type="text/css">{`${post.codeinjection_styles}`}</style>
             </Helmet>
             <Layout>
+                <div className="light-box" id="lbox" onClick={removeLightBox}>
+                    <span
+                        style={{
+                            backgroundImage: `url(${galleryImg})`,
+                        }}
+                    ></span>
+                </div>
                 <div className="container">
                     <article className="content">
-                        { post.feature_image ?
+                        {post.feature_image ? (
                             <figure className="post-feature-image">
-                                <img src={ post.feature_image } alt={ post.title } />
-                            </figure> : null }
+                                <img
+                                    src={post.feature_image}
+                                    alt={post.title}
+                                />
+                            </figure>
+                        ) : null}
                         <section className="post-full-content">
                             <h1 className="content-title">{post.title}</h1>
 
-                            {/* The main post content */ }
+                            {/* The main post content */}
                             <section
                                 className="content-body load-external-scripts"
                                 dangerouslySetInnerHTML={{ __html: post.html }}
+                                onClick={(e) => clickRecord(e)}
                             />
                         </section>
                     </article>
                 </div>
             </Layout>
         </>
-    )
+    );
 }
 
 Post.propTypes = {
