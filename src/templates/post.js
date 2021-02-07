1865c7fa261add015cca2ecdf1d9e1f60965764d
Yoan Ante <yoanante@gmail.com>
2021-02-07 16:52:00 -0500

    test netlify deployment

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet'
 
 import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
-import Form from '../components/common/Form'
+// import Form from '../components/common/Form'
 import { set } from 'lodash'
 
 /**
@@ -65,7 +65,7 @@ const Post = ({ data, location }) => {
                             />
                         </section>
                     </article>
-                    <Form />
+                    {/* <Form /> */}
                 </div>
             </Layout>
         </>
